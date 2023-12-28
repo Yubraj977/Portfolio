c639b2bede588a749b549191681afda7cdd8012c
Yubraj977 <yubrajkhatri977@gmail.com>
2023-12-28 15:17:05 +0545

    The first setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>This is the portfolio that is in its building phase feel free to contribute</h1>
+      <h2><a href="https://github.com/Yubraj977/Portfolio">click here to contribute</a></h2>
     </>
   )
 }
