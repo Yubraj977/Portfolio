258afe3cb45cd40adb26c84bfb2339ad3799e676
Yubraj977 <yubrajkhatri977@gmail.com>
2024-01-21 00:06:17 +0545

    some costumization

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -4,14 +4,18 @@ import { Outlet,NavLink } from 'react-router-dom'
 export default function Nav() {
   return (
     <>
-    <header className='flex justify-between items-center bg-slate-200 px-10' >
-      <h1><img src={logo} alt="Logo" className='h-12 ml-25'/></h1>
-       <nav>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to='/about' className='ml-5'>About</NavLink>
-        <NavLink to='/portfolio' className="ml-5">Portfolio</NavLink>
-        <NavLink to='/contact' className="ml-5">Contact</NavLink>
-        <NavLink to='/blog' className='ml-5'>Blog/Articles</NavLink>
+    <header className='bg-gray-200 fixed flex flex-col h-full items-center justify-center w-16' >
+      <h1><img src={logo} alt="Logo" className='h-12 absolute top-1 left-1'/></h1>
+       
+       
+       
+       <nav className='flex flex-col items-center gap-2'>
+
+        <NavLink to='/'><p className='text-2xl'><ion-icon name="home-outline"></ion-icon></p></NavLink>
+        <NavLink to='/about'><p className='text-2xl'><i class="fa-solid fa-user"></i></p></NavLink>
+        <NavLink to='/portfolio' ><p className='2xl'> <i class="fa-brands fa-creative-commons-nd"></i></p></NavLink>
+        <NavLink to='/contact' ><p className='text-2xl'><i class="fa-solid fa-phone"></i></p></NavLink>
+        <NavLink to='/blog' ><p className='text-2xl'><i class="fa-brands fa-creative-commons-nd"></i></p></NavLink>
        </nav>
     </header>
     <Outlet/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        pano: ["Panoptica W00 Script"]
+      }
+    },
+
   },
   plugins: [],
 }
