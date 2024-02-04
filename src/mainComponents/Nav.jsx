bfbf20643f1bc9b95cd3726e950e3ed3b7bb4e84
Yubraj977 <yubrajkhatri977@gmail.com>
2024-02-04 14:47:09 +0545

    other changes

--- src/mainComponents/Nav.jsx
+++ src/mainComponents/Nav.jsx
@@ -20,8 +20,8 @@ function Nav() {
     return (
         <div>
             <nav class=" fixed w-full top-0 z-10 bg-stone-700">
-                <div className='flex justify-between items-between  lg:px-4 md:px-4'>
-                    <div className="left font-bold text-2xl text-white"><span className='font-black text-green-700 text-3xl'>Yubraj</span> Khatri</div>
+                <div className='flex justify-between items-between px-2  lg:px-4 md:px-4'>
+                    <div className="left font-bold text-2xl text-white flex flex-col lg:flex-row"><span className='font-black text-green-700 text-3xl'>Yubraj</span> Khatri</div>
                     <div className="Middle  items-center gap-4 font-bold text-white hidden lg:flex md:flex" >
                         <div onClick={() => scrollToSection(home)}>Home</div>
                         <div onClick={() => scrollToSection(about)}>About</div>
@@ -31,7 +31,7 @@ function Nav() {
                         <div onClick={() => scrollToSection(projects)}>Projects</div>
                     </div>
 
-                    <div className="right flex gap-4 lg:mr-8 text-white text-sm justify-center items-center">
+                    <div className="right flex gap-4 mr-4 lg:mr-8 text-white text-sm justify-center items-center">
                         <div className='Twitter '>  <a href="https://twitter.com/YubrajK81639352" target='blank'> <span><i class="fa-brands fa-twitter"></i>  </span> <span className='mr-2'>Twitter</span></a> </div>
                         <div className='Github'>  <a href="https://github.com/Yubraj977" target='blank'> <span><i class="fa-brands fa-github"> </i> </span>  <span className='mr-2'>Github</span> </a> </div>
                         <div className='linkenden'> <a href="https://www.linkedin.com/in/yubraj-khatri-155786243/" target='blank'>  <span> <i class="fa-brands fa-linkedin"></i> </span> <span className='mr-2'>Linkenden</span> </a> </div>
